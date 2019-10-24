bbcbdda68e560a481accae02a0b69478a8567486
mcramirez16 <45410702+mcramirez16@users.noreply.github.com>
2019-10-24 13:44:33 -0400

    Results Constructors

--- Hmwk_1/src/RoboticsResult.java
+++ Hmwk_1/src/RoboticsResult.java
@@ -1,4 +1,22 @@
 
 public class RoboticsResult {
-
+	RoboticsTeam team1;
+	RoboticsTeam team2;
+	Double team1pts;
+	Double team2pts;
+	int team1tasks;
+	int team2tasks;
+	boolean team1fell;
+	boolean team2fell;
+	
+	public RoboticsResult(RoboticsTeam team1, RoboticsTeam team2, Double team1pts, Double team2pts, boolean team1fell, boolean team2fell, int team1tasks, int team2tasks) {
+		this.team1 = team1;
+		this.team2 = team2;
+		this.team1pts = team1pts;
+		this.team2pts = team2pts;
+		this.team1tasks = team1tasks;
+		this.team2tasks = team2tasks;
+		this.team1fell = team1fell;
+		this.team2fell = team2fell;
+	}
 }
--- Hmwk_1/src/RugbyResult.java
+++ Hmwk_1/src/RugbyResult.java
@@ -1,4 +1,15 @@
 
 public class RugbyResult {
-
+	RugbyTeam team1;
+	RugbyTeam team2;
+	
+	Double team1pts;
+	Double team2pts;
+	
+	public RugbyResult(RugbyTeam team1, RugbyTeam team2, Double team1pts, Double team2pts) {
+		this.team1 = team1;
+		this.team2 = team2;
+		this.team1pts = team1pts;
+		this.team2pts = team2pts;
+	}
 }
