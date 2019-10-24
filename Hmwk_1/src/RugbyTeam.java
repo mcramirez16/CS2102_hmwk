846f8d39d591dc1555d65e89b3c01444e0c9b70e
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-10-24 15:12:42 -0400

    Added class for rugby team
    for hw 1

--- Hmwk_1/src/RugbyTeam.java
+++ Hmwk_1/src/RugbyTeam.java
@@ -1,4 +1,13 @@
 
 public class RugbyTeam {
+	
+	String country;
+	String jerseyColor;
+	boolean hasRitual;
 
+	public RugbyTeam(String country, String jerseyColor, boolean hasRitual) {
+		this.country = country;
+		this.jerseyColor = jerseyColor;
+		this.hasRitual = hasRitual;
+	}
 }
