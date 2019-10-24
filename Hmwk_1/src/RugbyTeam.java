4d2e03ba6ffec9e4ba4533d78358cc2fff4083a0
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-10-24 15:25:01 -0400

    Added wins and losses to rugby team class
    for hw1

--- Hmwk_1/src/RugbyTeam.java
+++ Hmwk_1/src/RugbyTeam.java
@@ -4,10 +4,14 @@ public class RugbyTeam {
 	String country;
 	String jerseyColor;
 	boolean hasRitual;
+	int wins;
+	int losses;
 
-	public RugbyTeam(String country, String jerseyColor, boolean hasRitual) {
+	public RugbyTeam(String country, String jerseyColor, boolean hasRitual, int wins, int losses) {
 		this.country = country;
 		this.jerseyColor = jerseyColor;
 		this.hasRitual = hasRitual;
+		this.wins = wins;
+		this.losses = losses;
 	}
 }
