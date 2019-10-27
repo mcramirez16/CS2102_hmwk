f37a5b5cc7f456781b263c37cb6861eddc1a170b
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-10-26 22:39:31 -0400

    Minor Naming Change in Match

--- Hmwk_1/src/Match.java
+++ Hmwk_1/src/Match.java
@@ -5,7 +5,7 @@ public class Match {
 	RugbyResult RugbyGame;
 	RoboticsTeam School1;
 	RoboticsTeam School2;
-	RoboticsResult RoboticsComp;
+	RoboticsResult RobotsComp;
 	
 	public Match(RugbyTeam Country1, RugbyTeam Country2, RugbyResult RugbyGame) {
 		this.Country1 = Country1;
@@ -13,10 +13,10 @@ public class Match {
 		this.RugbyGame = RugbyGame;
 	}
 	
-	public Match(RoboticsTeam School1, RoboticsTeam School2, RoboticsResult RoboticsComp) {
+	public Match(RoboticsTeam School1, RoboticsTeam School2, RoboticsResult RobotsComp) {
 		this.School1 = School1;
 		this.School2 = School2;
-		this.RoboticsComp = RoboticsComp;
+		this.RobotsComp = RobotsComp;
 	}
 	
 }
