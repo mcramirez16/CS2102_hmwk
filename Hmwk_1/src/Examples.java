566f1799b629652af60cc878aab9482368828c1d
mcramirez16 <45410702+mcramirez16@users.noreply.github.com>
2019-10-28 18:29:23 -0400

    get Score Tests finished

--- Hmwk_1/src/Examples.java
+++ Hmwk_1/src/Examples.java
@@ -9,18 +9,19 @@ public class Examples {
 	RoboticsTeam robotteam1 = new RoboticsTeam("APECHS","arm",4);
 	RoboticsTeam robotteam2 = new RoboticsTeam("OHS","climber",2);
 	
-	RugbyResult invalidRugbyResult = new RugbyResult(rugbyteam1,rugbyteam2,170.0,14.0);
+	RugbyResult invalidRugbyResult = new RugbyResult(rugbyteam1,rugbyteam2,14.0,170.0);
 	RugbyResult validRugbyResult = new RugbyResult(rugbyteam1,rugbyteam2,14.0,10.0);
 	RugbyResult edgeRugbyResult = new RugbyResult(rugbyteam1,rugbyteam2,150.0,10.0);
+	
 	RoboticsResult invalidRobotResult = new RoboticsResult(robotteam1,robotteam2,40.0,12,false,5.0,1,true);
-	RoboticsResult validRobotResult = new RoboticsResult(robotteam1, robotteam2, 10.0, 5, true, 14.0, 3, true);
+	RoboticsResult validRobotResult = new RoboticsResult(robotteam1, robotteam2, 10.0, 5, false, 14.0, 3, false);
 	RoboticsResult edgeRobotResult = new RoboticsResult(robotteam1, robotteam2, 16.1, 8, true, 14.0, 3, true);
 	
 	
 	Match rugby_match = new Match(rugbyteam1,rugbyteam2,validRugbyResult);
 	Match robotics_match = new Match(robotteam1,robotteam2,validRobotResult);
 	
-
+	//isValid() Tests for Robotic Results
 	@Test
 	public void invalidRobotResultTest() {
 		assertFalse(invalidRobotResult.isValid());
@@ -36,6 +37,7 @@ public class Examples {
 		assertFalse(edgeRobotResult.isValid());
 	}
 	
+	//isValid() Tests for Rugby Results
 	@Test
 	public void invalidRugbyResultTest() {
 		assertFalse(invalidRugbyResult.isValid());
@@ -49,6 +51,27 @@ public class Examples {
 	@Test
 	public void edgeRugbyResultTest() {
 		assertFalse(edgeRugbyResult.isValid());
+	}
+	
+	//getScore() Tests
+	@Test
+	public void scoreRugbyResultTest1() {
+		assertEquals(validRugbyResult.getWinner(),rugbyteam1);
+	}
+	
+	@Test
+	public void scoreRugbyResultTest2() {
+		assertEquals(invalidRugbyResult.getWinner(),rugbyteam2);
+	}
+	
+	@Test
+	public void scoreRobotResultTest1() {
+		assertEquals(validRobotResult.getWinner(),robotteam2);
+	}
+	
+	@Test
+	public void scoreRobotResultTest2() {
+		assertEquals(edgeRobotResult.getWinner(),robotteam1);
 	}
 
 }
