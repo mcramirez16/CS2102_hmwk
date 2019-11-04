3c06f6a7e15a1fa0df99d9953e3b402c8bd49e6b
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-11-03 20:28:37 -0500

    Attempted finalWinnerIsValid but unsuccessful

--- Hmwk_1/src/Tournament.java
+++ Hmwk_1/src/Tournament.java
@@ -3,11 +3,16 @@ import java.util.LinkedList;
 public class Tournament {
 
 	LinkedList<IWinner> rounds;
+
 	public Tournament(LinkedList<IWinner> rounds) {
 		this.rounds = rounds;
 	}
-	
+
 	public boolean finalWinnerIsValid(IContestant team) {
-		return false;
+		for(int i = 0; i<rounds.size();i++) {
+			//if(rounds.get(i).isWinner(team).equals(team)) {
 	}
+}
+	
+
 }
