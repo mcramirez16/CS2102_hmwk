6f65866033bc40559274a87ceb5c0e6deb448ea3
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-11-11 21:13:03 -0500

    Update Earthquake2.java

--- Hmwk_3/src/Earthquake2.java
+++ Hmwk_3/src/Earthquake2.java
@@ -1,21 +1,27 @@
 import java.util.LinkedList;
 
 class Earthquake2 {
-  Earthquake2(){}
-      
-  // checks whether a datum is a date
-  boolean isDate(double anum) { return (int)anum > 10000000; }
-  // extracts the month from an 8-digit date
-  int extractMonth(double dateNum) { return ((int)dateNum % 10000) / 100; }
+	Earthquake2() {
+	}
 
-  public LinkedList<MaxHzReport> dailyMaxForMonth(LinkedList<Double> data, int month) {
-    
-	  Double maxRead = 0.0;
-	  LinkedList<MaxHzReport> maxData = new LinkedList<MaxHzReport>();
+	// checks whether a datum is a date
+	boolean isDate(double anum) {
+		return (int) anum > 10000000;
+	}
+
+	// extracts the month from an 8-digit date
+	int extractMonth(double dateNum) {
+		return ((int) dateNum % 10000) / 100;
+	}
+
+	public LinkedList<MaxHzReport> dailyMaxForMonth(LinkedList<Double> data, int month) {
+
+		Double maxRead = 0.0;
+		LinkedList<MaxHzReport> maxData = new LinkedList<MaxHzReport>();
 
 		for (double datum : data) {
 			if (!isDate(datum)) {
-				if(datum > maxRead) {
+				if (datum > maxRead) {
 					maxRead = datum;
 				} else {
 					data.remove(datum);
@@ -24,22 +30,22 @@ class Earthquake2 {
 				maxRead = 0.0;
 			}
 		}
-		
-		for(int i=0; i < data.size(); i++) {
-			if(isDate(data.get(i))) {
-				maxData.add(new MaxHzReport(i, i+1));
+
+		for (int i = 0; i < data.size() - 1; i++) {
+			if (isDate(data.get(i))) {
+				maxData.add(new MaxHzReport(i, i + 1));
 			}
 		}
-		
+
 //		System.out.println(maxData);
-		for(MaxHzReport aReport : maxData) {
-			if(extractMonth(aReport.date) != month) {
+		for (MaxHzReport aReport : maxData) {
+			if (extractMonth(aReport.date) != month) {
 				maxData.remove(aReport);
 			}
 		}
-		
+
 		return maxData;
-		
 
 	}
+
 }
