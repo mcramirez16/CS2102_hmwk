4478b3030d1b66f06e92f90f1d5c5f295e7b4e77
mcramirez16 <45410702+mcramirez16@users.noreply.github.com>
2019-11-13 16:05:16 -0500

    ShowManager 2 refined

--- Hmwk_3/src/ShowExamples.java
+++ Hmwk_3/src/ShowExamples.java
@@ -9,7 +9,7 @@ public class ShowExamples
 	ShowManager2 sm2= new ShowManager2();
 	LinkedList<Show> shows = new LinkedList<Show>();
 	ShowSummary report1 = new ShowSummary();
-	ShowSummary specialReport = new ShowSummary();
+	LinkedList<Show> specialList1 = new LinkedList<Show>();
 	
 	public ShowExamples()
 	{
@@ -20,7 +20,7 @@ public class ShowExamples
 		Show s1 = new Show("Star Trek", 1800, eps1, false);
 		shows.add(s1);
 		report1.primetime.add(s1);
-		specialReport.primetime.add(s1);
+		specialList1.add(s1);
 		
 		LinkedList<Episode> eps2 = new LinkedList<Episode>();
 		eps2.add(new Episode("Fear of a Bot Planet", 23));
@@ -30,7 +30,7 @@ public class ShowExamples
 		Show s2 = new Show("Futurama", 1900, eps2, false);
 		shows.add(s2);
 		report1.primetime.add(s2);
-		specialReport.primetime.add(s2);
+		specialList1.add(s2);
 		
 		LinkedList<Episode> eps3 = new LinkedList<Episode>();
 		eps3.add(new Episode("Yakko's World", 4));
@@ -39,12 +39,12 @@ public class ShowExamples
 		Show s3 = new Show("Animaniacs", 1630, eps3, false);
 		shows.add(s3);
 		report1.daytime.add(s3);
-		specialReport.daytime.add(s3);
+		specialList1.add(s3);
 		
 		LinkedList<Episode> special1 = new LinkedList<Episode>();
 		special1.add(new Episode("Muppets Christmas Special",3));
 		Show sp1 = new Show("Muppets Christmas Special",2200,special1,true);
-		specialReport.latenight.add(sp1);
+		specialList1.add(sp1);
 		
 		LinkedList<Episode> eps4 = new LinkedList<Episode>();
 		eps4.add(new Episode("The Letter W", 59));
@@ -53,6 +53,7 @@ public class ShowExamples
 		Show s4 = new Show("Sesame Street", 900, eps4, false);
 		shows.add(s4);
 		report1.daytime.add(s4);
+		specialList1.add(s4);
 	}
 	
 	@Test
@@ -63,24 +64,25 @@ public class ShowExamples
 	}
 	
 	@Test
-	public void sm2_instructorTest() {
-		ShowSummary report3 = sm2.organizeShows(shows);
+	public void sm2_specialTest1() {
+		ShowSummary report3 = sm2.organizeShows(specialList1);
 		assertEquals(report1,report3);
 	}
 	
 	/*
 	 * Subtasks for Problem 1 (Solution 1):
-	 * Go through shows in a loop
+	 * Go through shows list in a loop
 	 * Determine if the show falls in daytime,primetime or latenight
 	 * Determine if the show is a special
-	 * Add to list if these requirements are met
-	 * Put shows in a summary in the original order using the Strings obtained earlier
+	 * Add to ShowSummary if these requirements are met
 	 * */
 	
 	/*
 	 * Subtasks for Problem 1 (Solution 2):
-	 * Go through each and add each show in loop
-	 * If the show is not in daytime,primetime, or latenight remove it from the list
+	 * Go through shows list in a loop
+	 * If the show doesnt fall in daytime,primetime or latenight or is a special add to temporary list
+	 * Remove the unwanted data from the original list
+	 * Add list to new ShowSummary
 	 * 
 	 * */
 
