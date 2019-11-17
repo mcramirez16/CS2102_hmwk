bfc13f31098220302d747f59a086097e4fd29269
mcramirez16 <45410702+mcramirez16@users.noreply.github.com>
2019-11-17 18:46:43 -0500

    HeapChecker

---
+++ Hmwk_4/src/HeapChecker.java
@@ -0,0 +1,22 @@
+
+public class HeapChecker {
+
+	
+	
+	boolean addEltTester(IHeap hOrig, int elt, IBinTree hAdded) {
+		return false;
+
+		   
+
+	  }
+	
+	boolean remMinEltTester(IHeap hOrig, IBinTree hRemoved) {
+		return false;
+
+
+	  }
+}
+
+
+
+
