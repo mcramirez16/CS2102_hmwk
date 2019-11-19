3234d15de8f743d28b50cf6d3492704232ed1695
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-11-18 22:55:18 -0500

    Added headers for methods

--- Hmwk_4/src/HeapChecker.java
+++ Hmwk_4/src/HeapChecker.java
@@ -1,8 +1,6 @@
 
 public class HeapChecker {
 
-	
-	
 	public boolean addEltTester(IHeap hOrig, int elt, IBinTree hAdded) {
 		return false;
 
@@ -10,6 +8,23 @@ public class HeapChecker {
 
 	  }
 	
+	public boolean isHeap(IBinTree aBT) {// determines if a binary tree is a heap
+		
+	}
+	
+	public LinkedList<Integer> heapToList(IHeap aHP) {// converts a heap to a linked list
+		
+	}
+	
+	public LinkedList<Integer> btToList(IBinTree aBT){// converts a binary tree to a list
+		
+	}
+	
+	public boolean containSameElts(LinkedList<Integer> lOrig, LinkedList<Integer> lAdded) {// checks if the elements
+		//are the same and have the same number of occurrences except the added term
+		int origCounter = 0;
+		int addedCounter = 0;
+	}
 }
 
 
