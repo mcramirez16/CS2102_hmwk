0ed88c328eeae657477a620b36db7e3be516d607
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-11-20 22:13:09 -0500

    Added btToList

--- Hmwk_4/src/HeapChecker.java
+++ Hmwk_4/src/HeapChecker.java
@@ -9,15 +9,18 @@ public class HeapChecker {
 
 	  }
 	
-
-	public LinkedList<Integer> heapToList(IHeap aHP) {
-		return null;// converts a heap to a linked list
-		
-	}
 	
-	public LinkedList<Integer> btToList(IBinTree aBT){
-		return null;// converts a binary tree to a list
+	public LinkedList<Integer> btToList(IBinTree aBT){// converts a binary tree to a list
+		LinkedList<Integer> abtEltList = new LinkedList<Integer>();
 		
+		if(aBT.size() != 0) {
+			
+			abtEltList.add(aBT.getNode());
+			btToList(aBT.getLeft());
+			btToList(aBT.getRight());
+		}
+		
+		return abtEltList;
 	}
 	
 	public boolean containSameElts(LinkedList<Integer> lOrig, LinkedList<Integer> lAdded) {// checks if the elements
