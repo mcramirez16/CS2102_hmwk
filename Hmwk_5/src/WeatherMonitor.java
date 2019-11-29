55fff3b94904fba1d08635215c8b99445f225ff5
mcramirez16 <45410702+mcramirez16@users.noreply.github.com>
2019-11-29 16:38:12 -0500

    total rainfall first attempt, no tests yet

--- Hmwk_5/src/WeatherMonitor.java
+++ Hmwk_5/src/WeatherMonitor.java
@@ -3,5 +3,25 @@ import java.util.LinkedList;
 public class WeatherMonitor {
 	
 	LinkedList<DailyWeatherReport> dailyreports = new LinkedList<DailyWeatherReport>();
+	
+	public double totalRainfallForMonth(int month, int year) {
+		double total = 0.0;
+		for(int i =0;i<this.dailyreports.size();i++) {
+			if(this.dailyreports.get(i).date.YEAR==year) {
+				if(this.dailyreports.get(i).date.MONTH==month) {
+					total = calcRainfall(dailyreports.get(i));
+				}
+			}
+		}
+		return total;
+	}
+	
+	public double calcRainfall(DailyWeatherReport report) {
+		double rainfall = 0.0;
+		for(int i =0;i<report.rainfall.size();i++) {
+			rainfall+=report.rainfall.get(i);
+		}
+		return rainfall;
+	}
 
 }
