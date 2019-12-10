39b216578611984b2709b73ebef644959ceb5eca
mcramirez16 <45410702+mcramirez16@users.noreply.github.com>
2019-12-09 22:42:38 -0500

    voting machine first iteration

--- Hmwk_6/src/VotingMachine.java
+++ Hmwk_6/src/VotingMachine.java
@@ -16,17 +16,46 @@ public class VotingMachine {
 		    }
 		  }
 		  
-		  public void screen() throws UnknownCandidateException,CandidateExistsException {
+		  public void screen() throws UnknownCandidateException, DuplicateVotesException, CandidateExistsException {
 		    this.printBallot();
-		    System.out.println("Who do you want to vote for?");
-		    String candidate = keyboard.next();
-		    if(data.getCandidateList().contains(candidate)) {
-		    	throw new UnknownCandidateException(candidate);
+		    System.out.println("Who is your first choice vote?");
+		    String cand1 = keyboard.nextLine();
+		    System.out.println("Who is your second choice vote?");
+		    String cand2 = keyboard.nextLine();
+		    System.out.println("Who is your third choice vote?");
+		    String cand3 = keyboard.nextLine();
+		    try {
+		    data.processVote(cand1, cand2, cand3);
 		    }
-		    
-		    
-		    System.out.println("You voted for " + candidate);
+		    catch(UnknownCandidateException e) {
+		    	System.out.println("Do you wish to add candidate? Enter Y/N");
+		    		if(!data.getCandidateList().contains(cand1)) {
+		    			addWriteIn(cand1);
+		    		}
+		    		if(!data.getCandidateList().contains(cand2)) {
+		    			addWriteIn(cand2);
+		    		}
+		    		if(!data.getCandidateList().contains(cand3)) {
+		    			addWriteIn(cand3);
+		    		}
+		    	
+		    	
+		    }
+		    catch(DuplicateVotesException e) {
+		    	System.out.println("You can't vote for the same candidate twice.");
+		    }
+
+		    System.out.println("You voted for " + cand1+cand2+cand3);
 		  }
+		  
+		  public void addWriteIn(String name) throws CandidateExistsException{
+		    	try {
+			  data.addCandidate(name);
+		    	}catch(CandidateExistsException e) {
+		    		System.out.print("Candidate Already Exists");
+		    	}
+		    	
+		    }
 		  
 		//  public int countVotes(String forcand) {
 //		    int numvotes = 0;
