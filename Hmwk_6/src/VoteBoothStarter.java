35bb6420d2e90b36a02c4599d4d2c4cfa59c2deb
Jeffrey Jiyang Wu <jwu976@yahoo.com>
2019-12-09 22:01:03 -0500

    Big push

--- Hmwk_6/src/VoteBoothStarter.java
+++ Hmwk_6/src/VoteBoothStarter.java
@@ -84,6 +84,7 @@ class ElectionData {
 	  HashMap<String, Integer> totalScore = new HashMap<String, Integer>();
 	  
 	  Set<String> fcVotes = firstChoice.keySet();
+
 	  
 	  for(String key : fcVotes) {
 		  totalScore.put(key, firstChoice.get(key) * 3 + secondChoice.get(key) * 2 + thirdChoice.get(key));
@@ -101,29 +102,7 @@ class ElectionData {
 	  }
 	  
 	  return winner;
+	  
   }
   
-  public void printBallot() {
-    System.out.println("The candidates are ");
-    for (String s : ballot) {
-      System.out.println(s);
-    }
-  }
-  
-  public void screen() throws CandidateExistsException {
-    this.printBallot();
-    System.out.println("Who do you want to vote for?");
-    String candidate = keyboard.next();
-    addCandidate(candidate);
-    System.out.println("You voted for " + candidate);
-  }
-  
-//  public int countVotes(String forcand) {
-//    int numvotes = 0;
-//    for (String s : votes) {
-//      if (s.equals(forcand))
-//        numvotes = numvotes+1;
-//    }
-//    return numvotes;
-//    }
   }
