fef3f3692c0320da1b3334da4949e4f8db4521b2
mcramirez16 <45410702+mcramirez16@users.noreply.github.com>
2019-12-10 21:25:26 -0500

    Test Cases Passed

--- Hmwk_6/src/Examples.java
+++ Hmwk_6/src/Examples.java
@@ -1,8 +1,11 @@
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 
 public class Examples {
+	
 	ElectionData ballot = new ElectionData();
 	
 	public Examples() throws CandidateExistsException, UnknownCandidateException, DuplicateVotesException{
@@ -10,20 +13,103 @@ public class Examples {
 		ballot.addCandidate("Deku");
 		ballot.addCandidate("Shoto");
 		ballot.addCandidate("AllMight");
-		
-		ballot.processVote("Bakugo", "Deku", "AllMight");
-		ballot.processVote("Bakugo", "Shoto", "Deku");
-		ballot.processVote("Bakugo", "Deku", "AllMight");
 	}
 
 	@Test
-	public void mostFirstVotesTest() {
+	public void mostFirstVotesNormalTest() throws UnknownCandidateException, DuplicateVotesException {
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		ballot.processVote("Bakugo", "Shoto", "Deku");
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		
 		assertEquals(ballot.findWinnerMostFirstVotes(),"Bakugo");
 	}
 	
 	@Test
-	public void mostVotesTest() {
+	public void mostFirstVotesOtherCandTest() throws UnknownCandidateException, DuplicateVotesException {
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		ballot.processVote("Bakugo", "Shoto", "Deku");
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		
+		ballot.processVote("Deku", "Bakugo", "AllMight");
+		ballot.processVote("Deku", "Shoto", "Bakugo");
+		ballot.processVote("Deku", "Bakugo", "AllMight");
+		ballot.processVote("Deku", "Shoto", "Bakugo");
+		assertEquals(ballot.findWinnerMostFirstVotes(),"Deku");
+	}
+	
+	@Test
+	public void mostFirstVotesRunoffTest() throws UnknownCandidateException, DuplicateVotesException {
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		ballot.processVote("Bakugo", "Shoto", "Deku");
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		
+		ballot.processVote("Deku", "Bakugo", "AllMight");
+		ballot.processVote("Deku", "Shoto", "Bakugo");
+		ballot.processVote("Deku", "Bakugo", "AllMight");
+		assertEquals(ballot.findWinnerMostFirstVotes(),"Runoff required");
+	}
+	
+	@Test
+	public void mostVotesNormalTest() throws UnknownCandidateException, DuplicateVotesException {
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		ballot.processVote("Bakugo", "Shoto", "Deku");
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		
 		assertEquals(ballot.findWinnerMostPoints(),"Bakugo");
 	}
+	
+	@Test
+	public void mostVotesTest1() throws UnknownCandidateException, DuplicateVotesException {
+		boolean isWinner = false;
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		ballot.processVote("Bakugo", "Shoto", "Deku");
+		ballot.processVote("Bakugo", "Deku", "AllMight");
+		
+		ballot.processVote("Deku", "Shoto", "AllMight");
+		ballot.processVote("Shoto", "AllMight", "Deku");
+		LinkedList<String> winners = new LinkedList<String>();
+		winners.add("Deku");
+		winners.add("Bakugo");
+		if(winners.contains(ballot.findWinnerMostPoints())) {
+			isWinner = true;
+		}
+		assertTrue(isWinner);
+		
+	}
+	
+	@Test
+	public void mostVotesTest2() throws UnknownCandidateException, DuplicateVotesException {
+		boolean isWinner = false;
+		ballot.processVote("Deku", "Bakugo", "AllMight");
+		ballot.processVote("Deku", "Shoto", "Bakugo");
+		ballot.processVote("Deku", "Bakugo", "AllMight");
+		
+		ballot.processVote("Bakugo", "Shoto", "AllMight");
+		ballot.processVote("Shoto", "AllMight", "Deku");
+		LinkedList<String> winners = new LinkedList<String>();
+		winners.add("Deku");
+		winners.add("Bakugo");
+		if(winners.contains(ballot.findWinnerMostPoints())) {
+			isWinner = true;
+		}
+		assertTrue(isWinner);
+		
+	}
+	
+	@Test(expected=UnknownCandidateException.class)
+	public void unknownProcessVoteTest() throws UnknownCandidateException, DuplicateVotesException {
+		ballot.processVote("Hawks", "Bakugo", "Deku");
+	}
+	
+	@Test(expected=DuplicateVotesException.class)
+	public void duplicateProcessVoteTest() throws UnknownCandidateException, DuplicateVotesException {
+		ballot.processVote("Bakugo", "Bakugo", "Deku");
+	}
+	
+	@Test(expected=CandidateExistsException.class)
+	public void candidateExistsTest() throws CandidateExistsException {
+		ballot.addCandidate("Deku");
+	}
+	
 
 }
